70b90602c4085a0847290e8fb5924e752e9a23bb
Francine N <hello@francine.codes>
2014-11-22 21:33:00 -0500

    refactoring victories

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ toiletApp.ajaxRequest = function(country, apiID, apiKey){
 			toiletApp.parseCountryData(countryData);		
 		}
 	});
-}
+};
 
 toiletApp.listCountries = function(arr){
 	var random = Math.floor(Math.random()*(arr.length));
@@ -85,272 +85,98 @@ toiletApp.getCountryData = function(){
 	});
 };
 
-toiletApp.evalData = function(area,percent,id){
-	if (i==0){
+toiletApp.evalData = function(i, area, percent, countryData, id){
+	$chart = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
+	console.log(i, area, percent, id);
+	if (i===0){
 		// if first index matches second
-		if (area==countryData[1]['residence area']){
-			$('section.three, section.two').hide();
-			$('section.one').show();
-			var chart = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-			$('#area .chart').prepend(chart);
-			toiletApp.displayData();
-			$('#area').find('.percent').text(percent);
-			$('#area').find('.area').text(area);
-			break;
-
+		if (area===countryData[1]['residence area']&&area===countryData[2]['residence area']){
+				toiletApp.showOne(area, percent, $chart);
+				console.log('Match!');
+				console.log(countryData[1]['residence area']);
+				console.log(countryData[2]['residence area']);
 			// if first index matches third
-		}else if (area==countryData[2]['residence area']){
-			$('section.three, section.one').hide();
-			$('section.two').show();
-			var chart = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-			$('#area1 .chart').prepend(chart);
+		}else if (area===countryData[2]['residence area']){
+			toiletApp.showTwo();
+			$('#area1 .chart').prepend($chart);
 			toiletApp.displayData();
 			$('#area1').find('.percent').text(percent);
 			$('#area1').find('.area').text(area);
+		
 		}
 		// if first index is unique
-		else{
-			$('section.one, section.two').hide();
-			$('section.three').show();
-			var chart = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-			$('#'+id+' .chart').prepend(chart);
-			toiletApp.displayData();
-			$('#'+id).find('.percent').text(percent);
-			$('section.error').hide();
+		else if(area !== countryData[2]['residence area'] && area !== countryData[1]['residence area']){
+			toiletApp.showThree(area, percent, $chart, id);
+			console.log('No match!');
 		}
-	}else if(i==1){
-		if (countryData[0]['residence area']==countryData[2]['residence area']){
-			$('section.three, section.one').hide();
-			$('section.two').show();
-			var chart = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-			$('#area2 .chart').prepend(chart);
+	}else if(i===1){
+		if (countryData[0]['residence area']==countryData[2]['residence area'] && area != countryData[0]['residence area']){
+			toiletApp.showTwo();
+			$('#area2 .chart').prepend($chart);
 			toiletApp.displayData();
 			$('#area2').find('.percent').text(percent);
 			$('#area2').find('.area').text(area);
 		}
 		else{
-			$('section.one, section.two').hide();
-			$('section.three').show();
-			var chart = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-			$('#'+id+' .chart').prepend(chart);
-			toiletApp.displayData();
-			$('#'+id).find('.percent').text(percent);
-			$('section.error').hide();
+			toiletApp.showThree(area, percent, $chart, id);
 		}
 	}
-	else if (i==2){
-		$('section.two, section.one').hide();
-		$('section.three').show();
-		var chart = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-		$('#'+id+' .chart').prepend(chart);
-		toiletApp.displayData();
-		$('#'+id).find('.percent').text(percent);
-		$('section.error').hide();
+	else if (i===2){
+		if(area !== countryData[0]['residence area'] && area !== countryData[1]['residence area']){
+			toiletApp.showThree(area, percent, $chart, id);
+			console.log('No matches!');
+		}
 	}
-}
+};
+
+toiletApp.showOne = function(area, percent, chart){
+	$('section.one').show();
+	$('section.three, section.two').hide(); 
+	$('section.error').hide();
+	$('#area .chart').prepend(chart);
+	toiletApp.displayData();
+	$('#area').find('.percent').text(percent);
+	$('#area').find('.area').text(area);
+};
+
+toiletApp.showTwo = function(){
+	$('section.two').show();
+	$('section.three, section.one').hide();
+	$('section.error').hide();
+};
+
+toiletApp.showThree = function(area, percent, chart, id){
+	$('section.three').show();
+	$('section.one, section.two').hide(); 
+	$('section.error').hide();
+	$('#'+id+' .chart').prepend(chart);
+	toiletApp.displayData();
+	$('#'+id).find('.percent').text(percent);
+};
 
 toiletApp.parseCountryData = function(countryData){
-	if (countryData.length>=3){
-		for (var i=0;i<3;i++){
-			$('#countryName').text(countryData[i].area_name);
-			var area = countryData[i]['residence area'];
-			var percent = countryData[i].value;
-			if (area=='Total'){
-				if (i==0){
-					// if first index matches second
-					if (area==countryData[1]['residence area']){
-						$('section.three, section.two').hide();
-						$('section.one').show();
-						var total = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#area .chart').prepend(total);
-						toiletApp.displayData();
-						$('#area').find('.percent').text(percent);
-						$('#area').find('.area').text(area);
-						break;
-
-						// if first index matches third
-					}else if (area==countryData[2]['residence area']){
-						$('section.three, section.one').hide();
-						$('section.two').show();
-						var total = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#area1 .chart').prepend(total);
-						toiletApp.displayData();
-						$('#area1').find('.percent').text(percent);
-						$('#area1').find('.area').text(area);
-					}
-					// if first index is unique
-					else{
-						$('section.one, section.two').hide();
-						$('section.three').show();
-						var total = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#total .chart').prepend(total);
-						toiletApp.displayData();
-						$('#total').find('.percent').text(percent);
-						$('section.error').hide();
-					}
-				}else if(i==1){
-					if (countryData[0]['residence area']==countryData[2]['residence area']){
-						$('section.three, section.one').hide();
-						$('section.two').show();
-						var total = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#area2 .chart').prepend(total);
-						toiletApp.displayData();
-						$('#area2').find('.percent').text(percent);
-						$('#area2').find('.area').text(area);
-					}
-					else{
-						$('section.one, section.two').hide();
-						$('section.three').show();
-						var total = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#total .chart').prepend(total);
-						toiletApp.displayData();
-						$('#total').find('.percent').text(percent);
-						$('section.error').hide();
-					}
+	for (i=2;i>=0;i--){
+		if (countryData.length>=3){
+				$('#countryName').text(countryData[i].area_name);
+				var area = countryData[i]['residence area'];
+				var percent = countryData[i].value;
+				if (area==='Total'){
+					toiletApp.evalData(i, area, percent, countryData, 'total');
+				}else if (area==='Rural'){
+					toiletApp.evalData(i, area, percent, countryData, 'rural');
 				}
-				else if (i==2){
-					$('section.two, section.one').hide();
-					$('section.three').show();
-					var total = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-					$('#total .chart').prepend(total);
-					toiletApp.displayData();
-					$('#total').find('.percent').text(percent);
-					$('section.error').hide();
+				else if (area==='Urban'){
+					toiletApp.evalData(i, area, percent, countryData, 'urban');
 				}
-			}else if (area=='Rural'){
-				if (i==0){
-					if (area==countryData[1]['residence area']){
-						$('section.three, section.two').hide();
-						$('section.one').show();
-						var rural = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#area .chart').prepend(rural);
-						toiletApp.displayData();
-						$('#area').find('.percent').text(percent);
-						$('#area').find('.area').text(area);
-						break;
-					}else if (area==countryData[2]['residence area']){
-						$('section.three, section.one').hide();
-						$('section.two').show();
-						var rural = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#area1 .chart').prepend(rural);
-						toiletApp.displayData();
-						$('#area1').find('.percent').text(percent);
-						$('#area1').find('.area').text(area);
-					}
-					else{
-						$('section.one, section.two').hide();
-						$('section.three').show();
-						var rural = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#rural .chart').prepend(rural);
-						toiletApp.displayData();
-						$('#rural').find('.percent').text(percent);
-						$('section.error').hide();
-					}
-				}else if(i==1){
-					if (countryData[0]['residence area']==countryData[2]['residence area']){
-						$('section.three, section.one').hide();
-						$('section.two').show();
-						var rural = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#area2 .chart').prepend(rural);
-						toiletApp.displayData();
-						$('#area2').find('.percent').text(percent);
-						$('#area2').find('.area').text(area);
-					}
-					else{
-						$('section.one, section.two').hide();
-						$('section.three').show();
-						var rural = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#rural .chart').prepend(rural);
-						toiletApp.displayData();
-						$('#rural').find('.percent').text(percent);
-						$('section.error').hide();
-					}
-				}else if (i==2){
-					$('section.two, section.one').hide();
-					$('section.three').show();
-					var rural = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-					$('#rural .chart').prepend(rural);
-					toiletApp.displayData();
-					$('#rural').find('.percent').text(percent);
-					$('section.error').hide();
-				}
-			}
-			else if (area=='Urban'){
-				if (i==0){
-					if (area==countryData[1]['residence area']){
-						$('section.three, section.two').hide();
-						$('section.one').show();
-						var urban = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#area .chart').prepend(urban);
-						toiletApp.displayData();
-						$('#area').find('.percent').text(percent);
-						$('#area').find('.area').text(area);
-						break;
-					}else if (area==countryData[2]['residence area']){
-						$('section.three, section.one').hide();
-						$('section.two').show();
-						var urban = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#area1 .chart').prepend(urban);
-						toiletApp.displayData();
-						$('#area1').find('.percent').text(percent);
-						$('#area1').find('.area').text(area);
-					}
-					else{
-						$('section.one, section.two').hide();
-						$('section.three').show();
-						var urban = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#urban .chart').prepend(urban);
-						toiletApp.displayData();
-						$('#urban').find('.percent').text(percent);
-						$('section.countryData').show();
-						$('section.error').hide();
-					}
-				}else if(i==1){
-					if (countryData[0]['residence area']==countryData[2]['residence area']){
-						$('section.three, section.one').hide();
-						$('section.two').show();
-						var urban = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#area2 .chart').prepend(urban);
-						toiletApp.displayData();
-						$('#area2').find('.percent').text(percent);
-						$('#area2').find('.area').text(area);
-					}
-					else{
-						$('section.one, section.two').hide();
-						$('section.three').show();
-						var urban = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-						$('#urban .chart').prepend(urban);
-						toiletApp.displayData();
-						$('#urban').find('.percent').text(percent);
-						$('section.error').hide();
-					}
-				}else if (i==2){
-					$('section.two, section.one').hide();
-					$('section.three').show();
-					var urban = $('<input>').addClass('dial').attr({'type':'text', 'value':percent});
-					$('#urban .chart').prepend(urban);
-					toiletApp.displayData();
-					$('#urban').find('.percent').text(percent);
-					$('section.error').hide();
-				}
-			};
-		}; //END OF FOR LOOP
-	} 
-	//END OF IF STATEMENT
-	else{
-		$('section.one, section.two, section.three').hide();
-		$('section.error').show();
-		$('#countryNameError').text(countryData[i].area_name);	
-	};
+			
+		} //END OF IF STATEMENT
+		else{
+			$('section.one, section.two, section.three').hide();
+			$('section.error').show();
+			$('#countryNameError').text(countryData[i].area_name);	
+		}
+	} //END OF FOR LOOP
 }; //END OF METHOD
-
-		
-
-
-
-
-
-
 	
 $(function(){
 	toiletApp.init();
